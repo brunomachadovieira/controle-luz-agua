66a3a42cea80c1d69521671327c867e1dd42d314
Bruno Machado <96588962+brunomachadovieira@users.noreply.github.com>
2021-12-25 22:36:40 -0300

    Primeiro Commit

---
+++ dados.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tabela_Agua (
+    data_medidor integer,
+    numero_medidor integer
+);
+
+CREATE TABLE tabela_Luz (
+    data_medidor integer,
+    numero_medidor integer
+);
+
+
+INSERT INTO tabela_Agua VALUES (1623639600, 469);
+INSERT INTO tabela_Agua VALUES (1626231600, 479);
+INSERT INTO tabela_Agua VALUES (1634266800, 521);
+INSERT INTO tabela_Agua VALUES (1636686000, 537);
+INSERT INTO tabela_Agua VALUES (1639364400, 553);
+INSERT INTO tabela_Agua VALUES (1639882800, 556);
+INSERT INTO tabela_Luz VALUES (1636426800, 45030);
+INSERT INTO tabela_Luz VALUES (1639105200, 45422);
+INSERT INTO tabela_Luz VALUES (1639882800, 45544);
